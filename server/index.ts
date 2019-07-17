d4eba5a4f99efab0c33f654e6e233f29e64203bc
이강현 <khlee92@station3.co.kr>
2019-07-17 14:44:23 +0900

    ts first init

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,18 @@
-import * as tf from '@tensorflow/tfjs'
+const tf = require('@tensorflow/tfjs')
 
-const model: tf.Sequential = tf.sequential()
-model.add(tf.layers.dense({ units: 1, inputShape: [1] }))
+require('@tensorflow/tfjs-node')
 
-model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' })
+const model = tf.sequential()
+model.add(tf.layers.dense({units: 100, activation: 'relu', inputShape: [10]}))
+model.add(tf.layers.dense({units: 1, activation: 'linear'}))
+model.compile({optimizer: 'sgd', loss: 'meanSquaredError'});
 
-const xs = tf.tensor2d([1, 2, 3, 4], [4, 1])
-const ys = tf.tensor2d([1, 3, 5, 7], [4, 1])
+const xs = tf.randomNormal([100, 10])
+const ys = tf.randomNormal([100, 1])
 
-model.fit(xs, ys, { epochs: 10 }).then(() => {
-  const MD = model.predict(tf.tensor2d([5], [1, 1]))
-  console.log(MD)
+model.fit(xs, ys, {
+  epochs: 100,
+  callbacks: {
+    onEpochEnd: (epoch: any, log: any) => console.log(`Epoch ${epoch}: loss = ${log.loss}`)
+  }
 })
